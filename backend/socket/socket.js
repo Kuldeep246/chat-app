79c6d7b86f05b0133d6f28669f73cd7f732e129d
Kuldeep246 <122444111+Kuldeep246@users.noreply.github.com>
2024-02-05 22:19:56 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:5173"],
+		origin: ["https://chat-app-o5r0.onrender.com/"],
 		methods: ["GET", "POST"],
 	},
 });
